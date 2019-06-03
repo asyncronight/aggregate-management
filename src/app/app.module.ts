8fd932c017d309f900da0b02430c8991dc123cb8
salah3x <salah.loukili@gmail.com>
2019-06-03 14:32:51 +0000

    Add angular material Closes #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  exports: [MatButtonModule]
+})
+export class MaterialModule {}
