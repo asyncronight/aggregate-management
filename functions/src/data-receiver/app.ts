3185bcbb2fe89c600c3812debb91b8683312d6ea
salah3x <salah.loukili@gmail.com>
2019-06-05 01:02:30 +0000

    Change backend url

--- functions/src/data-receiver/app.ts
+++ functions/src/data-receiver/app.ts
@@ -15,12 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 // Create an express Router and define the endpoints
 const route: express.Router = express.Router();
 /**
- * POST: /api/data-receiver/data
+ * POST: /api/data
  * Save data to firestore
  * Expected data:
  */
 route.post(
-  '/data',
+  '',
   async (
     req: express.Request,
     res: express.Response,
@@ -31,7 +31,7 @@ route.post(
   }
 );
 // Register the router in the express app
-app.use('/api/data-receiver', route);
+app.use('/api/data', route);
 // Add a 404 handler
 app.all('**', (_, res) => res.status(404).send({ message: 'URL Not Found' }));
 // Add error handler
