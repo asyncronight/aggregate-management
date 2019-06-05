4d84243caec917ed4ab0662dec4cef7a7e85d1b7
salah3x <salah.loukili@gmail.com>
2019-06-05 00:59:14 +0000

    Enhance backend with 404 and error handlers plus body parsers

--- functions/src/data-receiver/app.ts
+++ functions/src/data-receiver/app.ts
@@ -9,6 +9,9 @@ const corsOptions = {
   origin: 'http://localhost:4200'
 };
 app.use(cors(corsOptions));
+// Add body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // Create an express Router and define the endpoints
 const route: express.Router = express.Router();
 /**
@@ -16,7 +19,30 @@ const route: express.Router = express.Router();
  * Save data to firestore
  * Expected data:
  */
-route.post('/data', async (req: express.Request, res: express.Response) => {
-  res.send('Hello');
-});
+route.post(
+  '/data',
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.log({ data: req.body });
+    res.status(200).json({ data: req.body });
+  }
+);
+// Register the router in the express app
 app.use('/api/data-receiver', route);
+// Add a 404 handler
+app.all('**', (_, res) => res.status(404).send({ message: 'URL Not Found' }));
+// Add error handler
+app.use(
+  (
+    error: Error,
+    _: express.Request,
+    res: express.Response,
+    __: express.NextFunction
+  ) => {
+    console.error('Something failed!', error);
+    res.status(500).json({ error });
+  }
+);
