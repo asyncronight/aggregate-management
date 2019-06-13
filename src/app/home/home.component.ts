405f70fa24e2048b3b05b3ad65efac5d90f4fe09
salah3x <salah.loukili@gmail.com>
2019-06-13 17:21:26 +0100

    Add home page Closes #12

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
+    (this.renderer.selectRootElement(
+      'mat-sidenav-content',
+      true
+    ) as HTMLElement).style.backgroundImage = 'url("/assets/btp.jpg';
+    (this.renderer.selectRootElement(
+      'mat-sidenav-content',
+      true
+    ) as HTMLElement).style.backgroundPosition = 'center';
+    (this.renderer.selectRootElement(
+      'mat-sidenav-content',
+      true
+    ) as HTMLElement).style.backgroundSize = 'cover';
   }
 
+  ngOnDestroy() {
+    (this.renderer.selectRootElement(
+      'mat-sidenav-content',
+      true
+    ) as HTMLElement).style.backgroundImage = '';
+  }
 }
